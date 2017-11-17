AniLeo/llvm

commit 6a02a2e02e0529c472a0b4c56dc998ef1f25b8b5

[ThinLTO] Fix missing call graph edges for calls with bitcasts.
This change doesn't fix the root cause of the miscompile PR34966 as the root
cause is in the linker ld64. This change makes call graph more complete
allowing to have better module imports/exports.

rdar://problem/35344706

Reviewers: tejohnson

Reviewed By: tejohnson

Subscribers: mehdi_amini, inglorion, eraman, llvm-commits, hiraditya

Differential Revision: https://reviews.llvm.org/D39356

llvm-svn: 317853

lib/Analysis/ModuleSummaryAnalysis.cpp
@@ -243,6 +243,11 @@ computeFunctionSummary(ModuleSummaryIndex &Index, const Module &M,
 
       auto *CalledValue = CS.getCalledValue();
       auto *CalledFunction = CS.getCalledFunction();
+      if (CalledValue && !CalledFunction) {
+        CalledValue = CalledValue->stripPointerCastsNoFollowAliases();
+        // Stripping pointer casts can reveal a called function.
+        CalledFunction = dyn_cast<Function>(CalledValue);
+      }
       // Check if this is an alias to a function. If so, get the
       // called aliasee for the checks below.
       if (auto *GA = dyn_cast<GlobalAlias>(CalledValue)) {
@@ -275,9 +280,8 @@ computeFunctionSummary(ModuleSummaryIndex &Index, const Module &M,
         // Skip inline assembly calls.
         if (CI && CI->isInlineAsm())
           continue;
-        // Skip direct calls.
-        if (!CS.getCalledValue() || isa<Constant>(CS.getCalledValue()))
-          continue;
+        assert(CalledValue && !isa<Constant>(CalledValue) &&
+               "Expected indirect call");
 
         uint32_t NumVals, NumCandidates;
         uint64_t TotalCount;

test/Bitcode/thinlto-function-summary-callgraph-cast.ll
@@ -1,0 +1,31 @@
+; Test to check the callgraph for calls to casts.
+; RUN: opt -module-summary %s -o %t.o
+; RUN: llvm-bcanalyzer -dump %t.o | FileCheck %s
+; PR34966
+
+; CHECK:       <GLOBALVAL_SUMMARY_BLOCK
+; CHECK-NEXT:    <VERSION
+; "op7=1" is a call to "callee" function.
+; CHECK-NEXT:    <PERMODULE {{.*}} op7=1 op8=[[ALIASID:[0-9]+]]/>
+; CHECK-NEXT:    <PERMODULE {{.*}} op0=[[ALIASEEID:[0-9]+]]
+; CHECK-NEXT:    <ALIAS {{.*}} op0=[[ALIASID]] {{.*}} op2=[[ALIASEEID]]/>
+; CHECK-NEXT:  </GLOBALVAL_SUMMARY_BLOCK>
+
+; ModuleID = 'thinlto-function-summary-callgraph-cast.ll'
+target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-unknown-linux-gnu"
+
+define void @caller() {
+    call void bitcast (void (...)* @callee to void ()*)()
+    call void bitcast (void (...)* @analias to void ()*)()
+    ret void
+}
+
+declare void @callee(...)
+
+@analias = alias void (...), bitcast (void ()* @aliasee to void (...)*)
+
+define void @aliasee() {
+entry:
+    ret void
+}

commit 2dc856594479f4699b442c84b48e134e85e9d0ac

[ThinLTO] Remove too aggressive assertion in building function call graph.
The assertion was introduced in r317853 but there are cases when a call
isn't handled either as direct or indirect. In this case we add a
reference graph edge but not a call graph edge.

Reviewers: tejohnson

Reviewed By: tejohnson

Subscribers: mehdi_amini, inglorion, eraman, hiraditya, efriedma, llvm-commits

Differential Revision: https://reviews.llvm.org/D40056

llvm-svn: 318540

lib/Analysis/ModuleSummaryAnalysis.cpp
@@ -280,8 +280,9 @@ computeFunctionSummary(ModuleSummaryIndex &Index, const Module &M,
         // Skip inline assembly calls.
         if (CI && CI->isInlineAsm())
           continue;
-        assert(CalledValue && !isa<Constant>(CalledValue) &&
-               "Expected indirect call");
+        // Skip direct calls.
+        if (!CalledValue || isa<Constant>(CalledValue))
+          continue;
 
         uint32_t NumVals, NumCandidates;
         uint64_t TotalCount;

test/Bitcode/thinlto-function-summary-callgraph-cast.ll
@@ -5,8 +5,10 @@
 
 ; CHECK:       <GLOBALVAL_SUMMARY_BLOCK
 ; CHECK-NEXT:    <VERSION
-; "op7=1" is a call to "callee" function.
-; CHECK-NEXT:    <PERMODULE {{.*}} op7=1 op8=[[ALIASID:[0-9]+]]/>
+; "op7" is a call to "callee" function.
+; CHECK-NEXT:    <PERMODULE {{.*}} op7=3 op8=[[ALIASID:[0-9]+]]/>
+; "another_caller" has only references but no calls.
+; CHECK-NEXT:    <PERMODULE {{.*}} op4=3 {{.*}} op7={{[0-9]+}}/>
 ; CHECK-NEXT:    <PERMODULE {{.*}} op0=[[ALIASEEID:[0-9]+]]
 ; CHECK-NEXT:    <ALIAS {{.*}} op0=[[ALIASID]] {{.*}} op2=[[ALIASEEID]]/>
 ; CHECK-NEXT:  </GLOBALVAL_SUMMARY_BLOCK>
@@ -21,6 +23,12 @@ define void @caller() {
     ret void
 }
 
+define void @another_caller() {
+    ; Test calls that aren't handled either as direct or indirect.
+    call void select (i1 icmp eq (i32* @global, i32* null), void ()* @f, void ()* @g)()
+    ret void
+}
+
 declare void @callee(...)
 
 @analias = alias void (...), bitcast (void ()* @aliasee to void (...)*)
@@ -29,3 +37,7 @@ define void @aliasee() {
 entry:
     ret void
 }
+
+declare void @f()
+declare void @g()
+@global = extern_weak global i32