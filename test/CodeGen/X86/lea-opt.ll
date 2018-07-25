AniLeo/llvm

commit 88b1f1b6671fdff98e24db9ddcbe84dabf8db3e3

[X86] Use a shift plus an lea for multiplying by a constant that is a power of 2 plus 2/4/8.
The LEA allows us to combine an add and the multiply by 2/4/8 together so we just need a shift for the larger power of 2.

llvm-svn: 337875

lib/Target/X86/X86ISelLowering.cpp
@@ -33749,6 +33749,24 @@ static SDValue combineMulSpecial(uint64_t MulAmt, SDNode *N, SelectionDAG &DAG,
     return DAG.getNode(ISD::ADD, DL, VT, N->getOperand(0),
                        combineMulMulAddOrSub(9, 3, /*isAdd*/ true));
   }
+
+  // Another trick. If this is a power 2 + 2/4/8, we can use a shift followed
+  // by a single LEA.
+  // First check if this a sum of two power of 2s because that's easy. Then
+  // count how many zeros are up to the first bit.
+  // TODO: We can do this even without LEA at a cost of two shifts and an add.
+  if (isPowerOf2_64(MulAmt & (MulAmt - 1))) {
+    unsigned ScaleShift = countTrailingZeros(MulAmt);
+    if (ScaleShift >= 1 && ScaleShift < 4) {
+      unsigned ShiftAmt = Log2_64((MulAmt & (MulAmt - 1)));
+      SDValue Shift1 = DAG.getNode(ISD::SHL, DL, VT, N->getOperand(0),
+                                   DAG.getConstant(ShiftAmt, DL, MVT::i8));
+      SDValue Shift2 = DAG.getNode(ISD::SHL, DL, VT, N->getOperand(0),
+                                   DAG.getConstant(ScaleShift, DL, MVT::i8));
+      return DAG.getNode(ISD::ADD, DL, VT, Shift1, Shift2);
+    }
+  }
+
   return SDValue();
 }
 

test/CodeGen/X86/mul-constant-i16.ll
@@ -705,11 +705,10 @@ define i16 @test_mul_by_62(i16 %x) {
 define i16 @test_mul_by_66(i16 %x) {
 ; X86-LABEL: test_mul_by_66:
 ; X86:       # %bb.0:
-; X86-NEXT:    movl {{[0-9]+}}(%esp), %ecx
-; X86-NEXT:    movl %ecx, %eax
-; X86-NEXT:    shll $6, %eax
-; X86-NEXT:    addl %ecx, %eax
-; X86-NEXT:    addl %ecx, %eax
+; X86-NEXT:    movl {{[0-9]+}}(%esp), %eax
+; X86-NEXT:    movl %eax, %ecx
+; X86-NEXT:    shll $6, %ecx
+; X86-NEXT:    leal (%ecx,%eax,2), %eax
 ; X86-NEXT:    # kill: def $ax killed $ax killed $eax
 ; X86-NEXT:    retl
 ;
@@ -718,8 +717,7 @@ define i16 @test_mul_by_66(i16 %x) {
 ; X64-NEXT:    # kill: def $edi killed $edi def $rdi
 ; X64-NEXT:    movl %edi, %eax
 ; X64-NEXT:    shll $6, %eax
-; X64-NEXT:    leal (%rax,%rdi), %eax
-; X64-NEXT:    addl %edi, %eax
+; X64-NEXT:    leal (%rax,%rdi,2), %eax
 ; X64-NEXT:    # kill: def $ax killed $ax killed $eax
 ; X64-NEXT:    retq
   %mul = mul nsw i16 %x, 66
@@ -743,6 +741,28 @@ define i16 @test_mul_by_73(i16 %x) {
 ; X64-NEXT:    # kill: def $ax killed $ax killed $eax
 ; X64-NEXT:    retq
   %mul = mul nsw i16 %x, 73
+  ret i16 %mul
+}
+
+define i16 @test_mul_by_520(i16 %x) {
+; X86-LABEL: test_mul_by_520:
+; X86:       # %bb.0:
+; X86-NEXT:    movl {{[0-9]+}}(%esp), %eax
+; X86-NEXT:    movl %eax, %ecx
+; X86-NEXT:    shll $9, %ecx
+; X86-NEXT:    leal (%ecx,%eax,8), %eax
+; X86-NEXT:    # kill: def $ax killed $ax killed $eax
+; X86-NEXT:    retl
+;
+; X64-LABEL: test_mul_by_520:
+; X64:       # %bb.0:
+; X64-NEXT:    # kill: def $edi killed $edi def $rdi
+; X64-NEXT:    movl %edi, %eax
+; X64-NEXT:    shll $9, %eax
+; X64-NEXT:    leal (%rax,%rdi,8), %eax
+; X64-NEXT:    # kill: def $ax killed $ax killed $eax
+; X64-NEXT:    retq
+  %mul = mul nsw i16 %x, 520
   ret i16 %mul
 }
 

test/CodeGen/X86/mul-constant-i32.ll
@@ -1836,11 +1836,10 @@ define i32 @test_mul_by_62(i32 %x) {
 define i32 @test_mul_by_66(i32 %x) {
 ; X86-LABEL: test_mul_by_66:
 ; X86:       # %bb.0:
-; X86-NEXT:    movl {{[0-9]+}}(%esp), %ecx
-; X86-NEXT:    movl %ecx, %eax
-; X86-NEXT:    shll $6, %eax
-; X86-NEXT:    addl %ecx, %eax
-; X86-NEXT:    addl %ecx, %eax
+; X86-NEXT:    movl {{[0-9]+}}(%esp), %eax
+; X86-NEXT:    movl %eax, %ecx
+; X86-NEXT:    shll $6, %ecx
+; X86-NEXT:    leal (%ecx,%eax,2), %eax
 ; X86-NEXT:    retl
 ;
 ; X64-HSW-LABEL: test_mul_by_66:
@@ -1848,8 +1847,7 @@ define i32 @test_mul_by_66(i32 %x) {
 ; X64-HSW-NEXT:    # kill: def $edi killed $edi def $rdi
 ; X64-HSW-NEXT:    movl %edi, %eax # sched: [1:0.25]
 ; X64-HSW-NEXT:    shll $6, %eax # sched: [1:0.50]
-; X64-HSW-NEXT:    leal (%rax,%rdi), %eax # sched: [1:0.50]
-; X64-HSW-NEXT:    addl %edi, %eax # sched: [1:0.25]
+; X64-HSW-NEXT:    leal (%rax,%rdi,2), %eax # sched: [1:0.50]
 ; X64-HSW-NEXT:    retq # sched: [7:1.00]
 ;
 ; X64-JAG-LABEL: test_mul_by_66:
@@ -1857,8 +1855,7 @@ define i32 @test_mul_by_66(i32 %x) {
 ; X64-JAG-NEXT:    # kill: def $edi killed $edi def $rdi
 ; X64-JAG-NEXT:    movl %edi, %eax # sched: [1:0.50]
 ; X64-JAG-NEXT:    shll $6, %eax # sched: [1:0.50]
-; X64-JAG-NEXT:    leal (%rax,%rdi), %eax # sched: [1:0.50]
-; X64-JAG-NEXT:    addl %edi, %eax # sched: [1:0.50]
+; X64-JAG-NEXT:    leal (%rax,%rdi,2), %eax # sched: [2:1.00]
 ; X64-JAG-NEXT:    retq # sched: [4:1.00]
 ;
 ; X86-NOOPT-LABEL: test_mul_by_66:
@@ -1940,6 +1937,63 @@ define i32 @test_mul_by_73(i32 %x) {
 ; SLM-NOOPT-NEXT:    imull $73, %edi, %eax # sched: [3:1.00]
 ; SLM-NOOPT-NEXT:    retq # sched: [4:1.00]
   %mul = mul nsw i32 %x, 73
+  ret i32 %mul
+}
+
+define i32 @test_mul_by_520(i32 %x) {
+; X86-LABEL: test_mul_by_520:
+; X86:       # %bb.0:
+; X86-NEXT:    movl {{[0-9]+}}(%esp), %eax
+; X86-NEXT:    movl %eax, %ecx
+; X86-NEXT:    shll $9, %ecx
+; X86-NEXT:    leal (%ecx,%eax,8), %eax
+; X86-NEXT:    retl
+;
+; X64-HSW-LABEL: test_mul_by_520:
+; X64-HSW:       # %bb.0:
+; X64-HSW-NEXT:    # kill: def $edi killed $edi def $rdi
+; X64-HSW-NEXT:    movl %edi, %eax # sched: [1:0.25]
+; X64-HSW-NEXT:    shll $9, %eax # sched: [1:0.50]
+; X64-HSW-NEXT:    leal (%rax,%rdi,8), %eax # sched: [1:0.50]
+; X64-HSW-NEXT:    retq # sched: [7:1.00]
+;
+; X64-JAG-LABEL: test_mul_by_520:
+; X64-JAG:       # %bb.0:
+; X64-JAG-NEXT:    # kill: def $edi killed $edi def $rdi
+; X64-JAG-NEXT:    movl %edi, %eax # sched: [1:0.50]
+; X64-JAG-NEXT:    shll $9, %eax # sched: [1:0.50]
+; X64-JAG-NEXT:    leal (%rax,%rdi,8), %eax # sched: [2:1.00]
+; X64-JAG-NEXT:    retq # sched: [4:1.00]
+;
+; X86-NOOPT-LABEL: test_mul_by_520:
+; X86-NOOPT:       # %bb.0:
+; X86-NOOPT-NEXT:    imull $520, {{[0-9]+}}(%esp), %eax # imm = 0x208
+; X86-NOOPT-NEXT:    retl
+;
+; HSW-NOOPT-LABEL: test_mul_by_520:
+; HSW-NOOPT:       # %bb.0:
+; HSW-NOOPT-NEXT:    imull $520, %edi, %eax # imm = 0x208
+; HSW-NOOPT-NEXT:    # sched: [3:1.00]
+; HSW-NOOPT-NEXT:    retq # sched: [7:1.00]
+;
+; JAG-NOOPT-LABEL: test_mul_by_520:
+; JAG-NOOPT:       # %bb.0:
+; JAG-NOOPT-NEXT:    imull $520, %edi, %eax # imm = 0x208
+; JAG-NOOPT-NEXT:    # sched: [3:1.00]
+; JAG-NOOPT-NEXT:    retq # sched: [4:1.00]
+;
+; X64-SLM-LABEL: test_mul_by_520:
+; X64-SLM:       # %bb.0:
+; X64-SLM-NEXT:    imull $520, %edi, %eax # imm = 0x208
+; X64-SLM-NEXT:    # sched: [3:1.00]
+; X64-SLM-NEXT:    retq # sched: [4:1.00]
+;
+; SLM-NOOPT-LABEL: test_mul_by_520:
+; SLM-NOOPT:       # %bb.0:
+; SLM-NOOPT-NEXT:    imull $520, %edi, %eax # imm = 0x208
+; SLM-NOOPT-NEXT:    # sched: [3:1.00]
+; SLM-NOOPT-NEXT:    retq # sched: [4:1.00]
+  %mul = mul nsw i32 %x, 520
   ret i32 %mul
 }
 

test/CodeGen/X86/mul-constant-i64.ll
@@ -1938,8 +1938,7 @@ define i64 @test_mul_by_66(i64 %x) {
 ; X86-NEXT:    movl {{[0-9]+}}(%esp), %eax
 ; X86-NEXT:    movl %eax, %ecx
 ; X86-NEXT:    shll $6, %ecx
-; X86-NEXT:    addl %eax, %ecx
-; X86-NEXT:    addl %eax, %ecx
+; X86-NEXT:    leal (%ecx,%eax,2), %ecx
 ; X86-NEXT:    movl $66, %eax
 ; X86-NEXT:    mull {{[0-9]+}}(%esp)
 ; X86-NEXT:    addl %ecx, %edx
@@ -1949,16 +1948,14 @@ define i64 @test_mul_by_66(i64 %x) {
 ; X64-HSW:       # %bb.0:
 ; X64-HSW-NEXT:    movq %rdi, %rax # sched: [1:0.25]
 ; X64-HSW-NEXT:    shlq $6, %rax # sched: [1:0.50]
-; X64-HSW-NEXT:    leaq (%rax,%rdi), %rax # sched: [1:0.50]
-; X64-HSW-NEXT:    addq %rdi, %rax # sched: [1:0.25]
+; X64-HSW-NEXT:    leaq (%rax,%rdi,2), %rax # sched: [1:0.50]
 ; X64-HSW-NEXT:    retq # sched: [7:1.00]
 ;
 ; X64-JAG-LABEL: test_mul_by_66:
 ; X64-JAG:       # %bb.0:
 ; X64-JAG-NEXT:    movq %rdi, %rax # sched: [1:0.50]
 ; X64-JAG-NEXT:    shlq $6, %rax # sched: [1:0.50]
-; X64-JAG-NEXT:    leaq (%rax,%rdi), %rax # sched: [1:0.50]
-; X64-JAG-NEXT:    addq %rdi, %rax # sched: [1:0.50]
+; X64-JAG-NEXT:    leaq (%rax,%rdi,2), %rax # sched: [2:1.00]
 ; X64-JAG-NEXT:    retq # sched: [4:1.00]
 ;
 ; X86-NOOPT-LABEL: test_mul_by_66:
@@ -2046,6 +2043,67 @@ define i64 @test_mul_by_73(i64 %x) {
 ; SLM-NOOPT-NEXT:    imulq $73, %rdi, %rax # sched: [3:1.00]
 ; SLM-NOOPT-NEXT:    retq # sched: [4:1.00]
   %mul = mul nsw i64 %x, 73
+  ret i64 %mul
+}
+
+define i64 @test_mul_by_520(i64 %x) {
+; X86-LABEL: test_mul_by_520:
+; X86:       # %bb.0:
+; X86-NEXT:    movl {{[0-9]+}}(%esp), %eax
+; X86-NEXT:    movl %eax, %ecx
+; X86-NEXT:    shll $9, %ecx
+; X86-NEXT:    leal (%ecx,%eax,8), %ecx
+; X86-NEXT:    movl $520, %eax # imm = 0x208
+; X86-NEXT:    mull {{[0-9]+}}(%esp)
+; X86-NEXT:    addl %ecx, %edx
+; X86-NEXT:    retl
+;
+; X64-HSW-LABEL: test_mul_by_520:
+; X64-HSW:       # %bb.0:
+; X64-HSW-NEXT:    movq %rdi, %rax # sched: [1:0.25]
+; X64-HSW-NEXT:    shlq $9, %rax # sched: [1:0.50]
+; X64-HSW-NEXT:    leaq (%rax,%rdi,8), %rax # sched: [1:0.50]
+; X64-HSW-NEXT:    retq # sched: [7:1.00]
+;
+; X64-JAG-LABEL: test_mul_by_520:
+; X64-JAG:       # %bb.0:
+; X64-JAG-NEXT:    movq %rdi, %rax # sched: [1:0.50]
+; X64-JAG-NEXT:    shlq $9, %rax # sched: [1:0.50]
+; X64-JAG-NEXT:    leaq (%rax,%rdi,8), %rax # sched: [2:1.00]
+; X64-JAG-NEXT:    retq # sched: [4:1.00]
+;
+; X86-NOOPT-LABEL: test_mul_by_520:
+; X86-NOOPT:       # %bb.0:
+; X86-NOOPT-NEXT:    movl $520, %eax # imm = 0x208
+; X86-NOOPT-NEXT:    mull {{[0-9]+}}(%esp)
+; X86-NOOPT-NEXT:    imull $520, {{[0-9]+}}(%esp), %ecx # imm = 0x208
+; X86-NOOPT-NEXT:    addl %ecx, %edx
+; X86-NOOPT-NEXT:    retl
+;
+; HSW-NOOPT-LABEL: test_mul_by_520:
+; HSW-NOOPT:       # %bb.0:
+; HSW-NOOPT-NEXT:    imulq $520, %rdi, %rax # imm = 0x208
+; HSW-NOOPT-NEXT:    # sched: [3:1.00]
+; HSW-NOOPT-NEXT:    retq # sched: [7:1.00]
+;
+; JAG-NOOPT-LABEL: test_mul_by_520:
+; JAG-NOOPT:       # %bb.0:
+; JAG-NOOPT-NEXT:    imulq $520, %rdi, %rax # imm = 0x208
+; JAG-NOOPT-NEXT:    # sched: [6:4.00]
+; JAG-NOOPT-NEXT:    retq # sched: [4:1.00]
+;
+; X64-SLM-LABEL: test_mul_by_520:
+; X64-SLM:       # %bb.0:
+; X64-SLM-NEXT:    imulq $520, %rdi, %rax # imm = 0x208
+; X64-SLM-NEXT:    # sched: [3:1.00]
+; X64-SLM-NEXT:    retq # sched: [4:1.00]
+;
+; SLM-NOOPT-LABEL: test_mul_by_520:
+; SLM-NOOPT:       # %bb.0:
+; SLM-NOOPT-NEXT:    imulq $520, %rdi, %rax # imm = 0x208
+; SLM-NOOPT-NEXT:    # sched: [3:1.00]
+; SLM-NOOPT-NEXT:    retq # sched: [4:1.00]
+  %mul = mul nsw i64 %x, 520
   ret i64 %mul
 }
 

commit 0bcf2cb3ebd386d267d03828868a7b91a55fa0ee

[X86] Autogenerate complete checks and fix a failure introduced in r337875.
llvm-svn: 337889

test/CodeGen/X86/lea-opt.ll
@@ -1,3 +1,4 @@
+; NOTE: Assertions have been autogenerated by utils/update_llc_test_checks.py
 ; RUN: llc < %s -mtriple=x86_64-linux | FileCheck %s -check-prefix=CHECK -check-prefix=ENABLED
 ; RUN: llc --disable-x86-lea-opt < %s -mtriple=x86_64-linux | FileCheck %s -check-prefix=CHECK -check-prefix=DISABLED
 
@@ -8,6 +9,50 @@
 @arr2 = external global [65 x %struct.anon2], align 16
 
 define void @test1(i64 %x) nounwind {
+; ENABLED-LABEL: test1:
+; ENABLED:       # %bb.0: # %entry
+; ENABLED-NEXT:    shlq $2, %rdi
+; ENABLED-NEXT:    movl arr1(%rdi,%rdi,2), %ecx
+; ENABLED-NEXT:    leaq arr1+4(%rdi,%rdi,2), %rax
+; ENABLED-NEXT:    subl arr1+4(%rdi,%rdi,2), %ecx
+; ENABLED-NEXT:    addl arr1+8(%rdi,%rdi,2), %ecx
+; ENABLED-NEXT:    cmpl $2, %ecx
+; ENABLED-NEXT:    je .LBB0_3
+; ENABLED-NEXT:  # %bb.1: # %entry
+; ENABLED-NEXT:    cmpl $1, %ecx
+; ENABLED-NEXT:    jne .LBB0_4
+; ENABLED-NEXT:  # %bb.2: # %sw.bb.1
+; ENABLED-NEXT:    movl $111, (%rax)
+; ENABLED-NEXT:    movl $222, 4(%rax)
+; ENABLED-NEXT:    retq
+; ENABLED-NEXT:  .LBB0_3: # %sw.bb.2
+; ENABLED-NEXT:    movl $333, (%rax) # imm = 0x14D
+; ENABLED-NEXT:    movl $444, 4(%rax) # imm = 0x1BC
+; ENABLED-NEXT:  .LBB0_4: # %sw.epilog
+; ENABLED-NEXT:    retq
+;
+; DISABLED-LABEL: test1:
+; DISABLED:       # %bb.0: # %entry
+; DISABLED-NEXT:    shlq $2, %rdi
+; DISABLED-NEXT:    movl arr1(%rdi,%rdi,2), %edx
+; DISABLED-NEXT:    leaq arr1+4(%rdi,%rdi,2), %rax
+; DISABLED-NEXT:    subl arr1+4(%rdi,%rdi,2), %edx
+; DISABLED-NEXT:    leaq arr1+8(%rdi,%rdi,2), %rcx
+; DISABLED-NEXT:    addl arr1+8(%rdi,%rdi,2), %edx
+; DISABLED-NEXT:    cmpl $2, %edx
+; DISABLED-NEXT:    je .LBB0_3
+; DISABLED-NEXT:  # %bb.1: # %entry
+; DISABLED-NEXT:    cmpl $1, %edx
+; DISABLED-NEXT:    jne .LBB0_4
+; DISABLED-NEXT:  # %bb.2: # %sw.bb.1
+; DISABLED-NEXT:    movl $111, (%rax)
+; DISABLED-NEXT:    movl $222, (%rcx)
+; DISABLED-NEXT:    retq
+; DISABLED-NEXT:  .LBB0_3: # %sw.bb.2
+; DISABLED-NEXT:    movl $333, (%rax) # imm = 0x14D
+; DISABLED-NEXT:    movl $444, (%rcx) # imm = 0x1BC
+; DISABLED-NEXT:  .LBB0_4: # %sw.epilog
+; DISABLED-NEXT:    retq
 entry:
   %a = getelementptr inbounds [65 x %struct.anon1], [65 x %struct.anon1]* @arr1, i64 0, i64 %x, i32 0
   %tmp = load i32, i32* %a, align 4
@@ -34,22 +79,53 @@ sw.bb.2:                                          ; preds = %entry
 
 sw.epilog:                                        ; preds = %sw.bb.2, %sw.bb.1, %entry
   ret void
-; CHECK-LABEL: test1:
-; CHECK:	shlq $2, [[REG1:%[a-z]+]]
-; CHECK:	movl arr1([[REG1]],[[REG1]],2), {{.*}}
-; CHECK:	leaq arr1+4([[REG1]],[[REG1]],2), [[REG2:%[a-z]+]]
-; CHECK:	subl arr1+4([[REG1]],[[REG1]],2), {{.*}}
-; DISABLED:	leaq arr1+8([[REG1]],[[REG1]],2), [[REG3:%[a-z]+]]
-; CHECK:	addl arr1+8([[REG1]],[[REG1]],2), {{.*}}
-; CHECK:	movl ${{[1-4]+}}, ([[REG2]])
-; ENABLED:	movl ${{[1-4]+}}, 4([[REG2]])
-; DISABLED:	movl ${{[1-4]+}}, ([[REG3]])
-; CHECK:	movl ${{[1-4]+}}, ([[REG2]])
-; ENABLED:	movl ${{[1-4]+}}, 4([[REG2]])
-; DISABLED:	movl ${{[1-4]+}}, ([[REG3]])
 }
 
 define void @test2(i64 %x) nounwind optsize {
+; ENABLED-LABEL: test2:
+; ENABLED:       # %bb.0: # %entry
+; ENABLED-NEXT:    shlq $2, %rdi
+; ENABLED-NEXT:    leaq arr1+4(%rdi,%rdi,2), %rax
+; ENABLED-NEXT:    movl -4(%rax), %ecx
+; ENABLED-NEXT:    subl (%rax), %ecx
+; ENABLED-NEXT:    addl 4(%rax), %ecx
+; ENABLED-NEXT:    cmpl $2, %ecx
+; ENABLED-NEXT:    je .LBB1_3
+; ENABLED-NEXT:  # %bb.1: # %entry
+; ENABLED-NEXT:    cmpl $1, %ecx
+; ENABLED-NEXT:    jne .LBB1_4
+; ENABLED-NEXT:  # %bb.2: # %sw.bb.1
+; ENABLED-NEXT:    movl $111, (%rax)
+; ENABLED-NEXT:    movl $222, 4(%rax)
+; ENABLED-NEXT:    retq
+; ENABLED-NEXT:  .LBB1_3: # %sw.bb.2
+; ENABLED-NEXT:    movl $333, (%rax) # imm = 0x14D
+; ENABLED-NEXT:    movl $444, 4(%rax) # imm = 0x1BC
+; ENABLED-NEXT:  .LBB1_4: # %sw.epilog
+; ENABLED-NEXT:    retq
+;
+; DISABLED-LABEL: test2:
+; DISABLED:       # %bb.0: # %entry
+; DISABLED-NEXT:    shlq $2, %rdi
+; DISABLED-NEXT:    movl arr1(%rdi,%rdi,2), %edx
+; DISABLED-NEXT:    leaq arr1+4(%rdi,%rdi,2), %rax
+; DISABLED-NEXT:    subl arr1+4(%rdi,%rdi,2), %edx
+; DISABLED-NEXT:    leaq arr1+8(%rdi,%rdi,2), %rcx
+; DISABLED-NEXT:    addl arr1+8(%rdi,%rdi,2), %edx
+; DISABLED-NEXT:    cmpl $2, %edx
+; DISABLED-NEXT:    je .LBB1_3
+; DISABLED-NEXT:  # %bb.1: # %entry
+; DISABLED-NEXT:    cmpl $1, %edx
+; DISABLED-NEXT:    jne .LBB1_4
+; DISABLED-NEXT:  # %bb.2: # %sw.bb.1
+; DISABLED-NEXT:    movl $111, (%rax)
+; DISABLED-NEXT:    movl $222, (%rcx)
+; DISABLED-NEXT:    retq
+; DISABLED-NEXT:  .LBB1_3: # %sw.bb.2
+; DISABLED-NEXT:    movl $333, (%rax) # imm = 0x14D
+; DISABLED-NEXT:    movl $444, (%rcx) # imm = 0x1BC
+; DISABLED-NEXT:  .LBB1_4: # %sw.epilog
+; DISABLED-NEXT:    retq
 entry:
   %a = getelementptr inbounds [65 x %struct.anon1], [65 x %struct.anon1]* @arr1, i64 0, i64 %x, i32 0
   %tmp = load i32, i32* %a, align 4
@@ -76,22 +152,6 @@ sw.bb.2:                                          ; preds = %entry
 
 sw.epilog:                                        ; preds = %sw.bb.2, %sw.bb.1, %entry
   ret void
-; CHECK-LABEL: test2:
-; CHECK:	shlq $2, [[REG1:%[a-z]+]]
-; DISABLED:	movl arr1([[REG1]],[[REG1]],2), {{.*}}
-; CHECK:	leaq arr1+4([[REG1]],[[REG1]],2), [[REG2:%[a-z]+]]
-; ENABLED:	movl -4([[REG2]]), {{.*}}
-; ENABLED:	subl ([[REG2]]), {{.*}}
-; ENABLED:	addl 4([[REG2]]), {{.*}}
-; DISABLED:	subl arr1+4([[REG1]],[[REG1]],2), {{.*}}
-; DISABLED:	leaq arr1+8([[REG1]],[[REG1]],2), [[REG3:%[a-z]+]]
-; DISABLED:	addl arr1+8([[REG1]],[[REG1]],2), {{.*}}
-; CHECK:	movl ${{[1-4]+}}, ([[REG2]])
-; ENABLED:	movl ${{[1-4]+}}, 4([[REG2]])
-; DISABLED:	movl ${{[1-4]+}}, ([[REG3]])
-; CHECK:	movl ${{[1-4]+}}, ([[REG2]])
-; ENABLED:	movl ${{[1-4]+}}, 4([[REG2]])
-; DISABLED:	movl ${{[1-4]+}}, ([[REG3]])
 }
 
 ; Check that LEA optimization pass takes into account a resultant address
@@ -99,6 +159,51 @@ sw.epilog:                                        ; preds = %sw.bb.2, %sw.bb.1, 
 ; address recalculation.
 
 define void @test3(i64 %x) nounwind optsize {
+; ENABLED-LABEL: test3:
+; ENABLED:       # %bb.0: # %entry
+; ENABLED-NEXT:    movq %rdi, %rax
+; ENABLED-NEXT:    shlq $7, %rax
+; ENABLED-NEXT:    leaq arr2+132(%rax,%rdi,8), %rcx
+; ENABLED-NEXT:    leaq arr2(%rax,%rdi,8), %rax
+; ENABLED-NEXT:    movl (%rcx), %edx
+; ENABLED-NEXT:    addl (%rax), %edx
+; ENABLED-NEXT:    cmpl $2, %edx
+; ENABLED-NEXT:    je .LBB2_3
+; ENABLED-NEXT:  # %bb.1: # %entry
+; ENABLED-NEXT:    cmpl $1, %edx
+; ENABLED-NEXT:    jne .LBB2_4
+; ENABLED-NEXT:  # %bb.2: # %sw.bb.1
+; ENABLED-NEXT:    movl $111, (%rcx)
+; ENABLED-NEXT:    movl $222, (%rax)
+; ENABLED-NEXT:    retq
+; ENABLED-NEXT:  .LBB2_3: # %sw.bb.2
+; ENABLED-NEXT:    movl $333, (%rcx) # imm = 0x14D
+; ENABLED-NEXT:    movl %eax, (%rax)
+; ENABLED-NEXT:  .LBB2_4: # %sw.epilog
+; ENABLED-NEXT:    retq
+;
+; DISABLED-LABEL: test3:
+; DISABLED:       # %bb.0: # %entry
+; DISABLED-NEXT:    movq %rdi, %rsi
+; DISABLED-NEXT:    shlq $7, %rsi
+; DISABLED-NEXT:    leaq arr2+132(%rsi,%rdi,8), %rcx
+; DISABLED-NEXT:    leaq arr2(%rsi,%rdi,8), %rax
+; DISABLED-NEXT:    movl arr2+132(%rsi,%rdi,8), %edx
+; DISABLED-NEXT:    addl arr2(%rsi,%rdi,8), %edx
+; DISABLED-NEXT:    cmpl $2, %edx
+; DISABLED-NEXT:    je .LBB2_3
+; DISABLED-NEXT:  # %bb.1: # %entry
+; DISABLED-NEXT:    cmpl $1, %edx
+; DISABLED-NEXT:    jne .LBB2_4
+; DISABLED-NEXT:  # %bb.2: # %sw.bb.1
+; DISABLED-NEXT:    movl $111, (%rcx)
+; DISABLED-NEXT:    movl $222, (%rax)
+; DISABLED-NEXT:    retq
+; DISABLED-NEXT:  .LBB2_3: # %sw.bb.2
+; DISABLED-NEXT:    movl $333, (%rcx) # imm = 0x14D
+; DISABLED-NEXT:    movl %eax, (%rax)
+; DISABLED-NEXT:  .LBB2_4: # %sw.epilog
+; DISABLED-NEXT:    retq
 entry:
   %a = getelementptr inbounds [65 x %struct.anon2], [65 x %struct.anon2]* @arr2, i64 0, i64 %x, i32 2
   %tmp = load i32, i32* %a, align 4
@@ -124,25 +229,57 @@ sw.bb.2:                                          ; preds = %entry
 
 sw.epilog:                                        ; preds = %sw.bb.2, %sw.bb.1, %entry
   ret void
-; CHECK-LABEL: test3:
-; CHECK:	imulq {{.*}}, [[REG1:%[a-z]+]]
-; CHECK:	leaq arr2+132([[REG1]]), [[REG2:%[a-z]+]]
-; CHECK:	leaq arr2([[REG1]]), [[REG3:%[a-z]+]]
 
 ; REG3's definition is closer to movl than REG2's, but the pass still chooses
 ; REG2 because it provides the resultant address displacement fitting 1 byte.
 
-; ENABLED:	movl ([[REG2]]), {{.*}}
-; ENABLED:	addl ([[REG3]]), {{.*}}
-; DISABLED:	movl arr2+132([[REG1]]), {{.*}}
-; DISABLED:	addl arr2([[REG1]]), {{.*}}
-; CHECK:	movl ${{[1-4]+}}, ([[REG2]])
-; CHECK:	movl ${{[1-4]+}}, ([[REG3]])
-; CHECK:	movl ${{[1-4]+}}, ([[REG2]])
-; CHECK:	movl {{.*}}, ([[REG3]])
 }
 
 define void @test4(i64 %x) nounwind minsize {
+; ENABLED-LABEL: test4:
+; ENABLED:       # %bb.0: # %entry
+; ENABLED-NEXT:    imulq $12, %rdi, %rax
+; ENABLED-NEXT:    leaq arr1+4(%rax), %rax
+; ENABLED-NEXT:    movl -4(%rax), %ecx
+; ENABLED-NEXT:    subl (%rax), %ecx
+; ENABLED-NEXT:    addl 4(%rax), %ecx
+; ENABLED-NEXT:    cmpl $2, %ecx
+; ENABLED-NEXT:    je .LBB3_3
+; ENABLED-NEXT:  # %bb.1: # %entry
+; ENABLED-NEXT:    cmpl $1, %ecx
+; ENABLED-NEXT:    jne .LBB3_4
+; ENABLED-NEXT:  # %bb.2: # %sw.bb.1
+; ENABLED-NEXT:    movl $111, (%rax)
+; ENABLED-NEXT:    movl $222, 4(%rax)
+; ENABLED-NEXT:    retq
+; ENABLED-NEXT:  .LBB3_3: # %sw.bb.2
+; ENABLED-NEXT:    movl $333, (%rax) # imm = 0x14D
+; ENABLED-NEXT:    movl $444, 4(%rax) # imm = 0x1BC
+; ENABLED-NEXT:  .LBB3_4: # %sw.epilog
+; ENABLED-NEXT:    retq
+;
+; DISABLED-LABEL: test4:
+; DISABLED:       # %bb.0: # %entry
+; DISABLED-NEXT:    imulq $12, %rdi, %rsi
+; DISABLED-NEXT:    movl arr1(%rsi), %edx
+; DISABLED-NEXT:    leaq arr1+4(%rsi), %rax
+; DISABLED-NEXT:    subl arr1+4(%rsi), %edx
+; DISABLED-NEXT:    leaq arr1+8(%rsi), %rcx
+; DISABLED-NEXT:    addl arr1+8(%rsi), %edx
+; DISABLED-NEXT:    cmpl $2, %edx
+; DISABLED-NEXT:    je .LBB3_3
+; DISABLED-NEXT:  # %bb.1: # %entry
+; DISABLED-NEXT:    cmpl $1, %edx
+; DISABLED-NEXT:    jne .LBB3_4
+; DISABLED-NEXT:  # %bb.2: # %sw.bb.1
+; DISABLED-NEXT:    movl $111, (%rax)
+; DISABLED-NEXT:    movl $222, (%rcx)
+; DISABLED-NEXT:    retq
+; DISABLED-NEXT:  .LBB3_3: # %sw.bb.2
+; DISABLED-NEXT:    movl $333, (%rax) # imm = 0x14D
+; DISABLED-NEXT:    movl $444, (%rcx) # imm = 0x1BC
+; DISABLED-NEXT:  .LBB3_4: # %sw.epilog
+; DISABLED-NEXT:    retq
 entry:
   %a = getelementptr inbounds [65 x %struct.anon1], [65 x %struct.anon1]* @arr1, i64 0, i64 %x, i32 0
   %tmp = load i32, i32* %a, align 4
@@ -169,20 +306,4 @@ sw.bb.2:                                          ; preds = %entry
 
 sw.epilog:                                        ; preds = %sw.bb.2, %sw.bb.1, %entry
   ret void
-; CHECK-LABEL: test4:
-; CHECK:	imulq {{.*}}, [[REG1:%[a-z]+]]
-; DISABLED:	movl arr1([[REG1]]), {{.*}}
-; CHECK:	leaq arr1+4([[REG1]]), [[REG2:%[a-z]+]]
-; ENABLED:	movl -4([[REG2]]), {{.*}}
-; ENABLED:	subl ([[REG2]]), {{.*}}
-; ENABLED:	addl 4([[REG2]]), {{.*}}
-; DISABLED:	subl arr1+4([[REG1]]), {{.*}}
-; DISABLED:	leaq arr1+8([[REG1]]), [[REG3:%[a-z]+]]
-; DISABLED:	addl arr1+8([[REG1]]), {{.*}}
-; CHECK:	movl ${{[1-4]+}}, ([[REG2]])
-; ENABLED:	movl ${{[1-4]+}}, 4([[REG2]])
-; DISABLED:	movl ${{[1-4]+}}, ([[REG3]])
-; CHECK:	movl ${{[1-4]+}}, ([[REG2]])
-; ENABLED:	movl ${{[1-4]+}}, 4([[REG2]])
-; DISABLED:	movl ${{[1-4]+}}, ([[REG3]])
 }